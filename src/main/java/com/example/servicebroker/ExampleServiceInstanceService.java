498ec20061185b800966f34791969cf06a697d22
Mukul Kansal <kansalmukul@gmail.com>
2020-06-11 14:07:29 +0530

    added

---
+++ src/main/java/com/example/servicebroker/ExampleServiceInstanceService.java
@@ -0,0 +1,97 @@
+package com.example.servicebroker;
+
+import java.util.Map;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceRequest;
+import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceResponse;
+import org.springframework.cloud.servicebroker.model.instance.DeleteServiceInstanceRequest;
+import org.springframework.cloud.servicebroker.model.instance.DeleteServiceInstanceResponse;
+import org.springframework.cloud.servicebroker.model.instance.GetLastServiceOperationRequest;
+import org.springframework.cloud.servicebroker.model.instance.GetLastServiceOperationResponse;
+import org.springframework.cloud.servicebroker.model.instance.GetServiceInstanceRequest;
+import org.springframework.cloud.servicebroker.model.instance.GetServiceInstanceResponse;
+import org.springframework.cloud.servicebroker.model.instance.OperationState;
+import org.springframework.cloud.servicebroker.model.instance.UpdateServiceInstanceRequest;
+import org.springframework.cloud.servicebroker.model.instance.UpdateServiceInstanceResponse;
+import org.springframework.cloud.servicebroker.service.ServiceInstanceService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleServiceInstanceService implements ServiceInstanceService {
+
+	@Override
+	public Mono<CreateServiceInstanceResponse> createServiceInstance(CreateServiceInstanceRequest request) {
+		String serviceInstanceId = request.getServiceInstanceId();
+		String planId = request.getPlanId();
+		Map<String, Object> parameters = request.getParameters();
+		System.out.println("*********************************createServiceInstance************************************8");
+
+		//
+		// perform the steps necessary to initiate the asynchronous
+		// provisioning of all necessary resources
+		//
+		
+		String dashboardUrl = ""; /* construct a dashboard URL */
+
+		return Mono.just(CreateServiceInstanceResponse.builder().dashboardUrl(dashboardUrl).async(true).build());
+	}
+
+	@Override
+	public Mono<UpdateServiceInstanceResponse> updateServiceInstance(UpdateServiceInstanceRequest request) {
+		String serviceInstanceId = request.getServiceInstanceId();
+		String planId = request.getPlanId();
+		String previousPlan = request.getPreviousValues().getPlanId();
+		Map<String, Object> parameters = request.getParameters();
+		System.out.println("*********************************updateServiceInstance************************************8");
+
+		//
+		// perform the steps necessary to initiate the asynchronous
+		// updating of all necessary resources
+		//
+
+		return Mono.just(UpdateServiceInstanceResponse.builder().async(true).build());
+	}
+
+	@Override
+	public Mono<DeleteServiceInstanceResponse> deleteServiceInstance(DeleteServiceInstanceRequest request) {
+		String serviceInstanceId = request.getServiceInstanceId();
+		String planId = request.getPlanId();
+		System.out.println("*********************************deleteServiceInstance************************************8");
+
+		//
+		// perform the steps necessary to initiate the asynchronous
+		// deletion of all provisioned resources
+		//
+
+		return Mono.just(DeleteServiceInstanceResponse.builder().async(true).build());
+	}
+
+	@Override
+	public Mono<GetServiceInstanceResponse> getServiceInstance(GetServiceInstanceRequest request) {
+		String serviceInstanceId = request.getServiceInstanceId();
+		System.out.println("*********************************getServiceInstance************************************8");
+
+		//
+		// retrieve the details of the specified service instance
+		//
+		String appUri = "http://mkt-service-app-" + serviceInstanceId+ ".ocp-ic-test.kubecf.dev";
+
+		String dashboardUrl = appUri; /* retrieve dashboard URL */
+
+		return Mono.just(GetServiceInstanceResponse.builder().dashboardUrl(dashboardUrl).build());
+	}
+
+	@Override
+	public Mono<GetLastServiceOperationResponse> getLastOperation(GetLastServiceOperationRequest request) {
+		String serviceInstanceId = request.getServiceInstanceId();
+		System.out.println("*********************************getLastOperation************************************8");
+
+		//
+		// determine the status of the operation in progress
+		//
+
+		return Mono.just(GetLastServiceOperationResponse.builder().operationState(OperationState.SUCCEEDED).build());
+	}
+}
---
+++ src/main/java/com/example/servicebroker/ServiceBrokerApplication.java
@@ -0,0 +1,15 @@
+package com.example.servicebroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBrokerApplication {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBrokerApplication.class, args);
+	}
+
+	
+}
